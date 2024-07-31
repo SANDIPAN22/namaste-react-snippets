a084299c6b8461995211353b17e2ba8fb4e1ba8f
Sandipan Chakraborty <chak.sandipan22@gmail.com>
2024-07-31 22:12:18 +0530

    day 1

---
+++ Run React using CDN/app.js
@@ -0,0 +1,16 @@
+const block = React.createElement("div", { id: "parent" }, [
+  React.createElement(
+    "div",
+    { id: "child-1" },
+    React.createElement("h1", {}, "H1 from child1")
+  ),
+  React.createElement(
+    "div",
+    { id: "child-2" },
+    React.createElement("h1", {}, "H1 from child2")
+  ),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(block);
